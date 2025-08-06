dd93074962c673fce8eee17ea874587aded26b9f
A Abhinav Jaisiv <aryanabhinav01@gmail.com>
2025-08-05 21:54:53 -0400

    feat: added aws provider configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+provider "aws" {
+  region = "us-east-1"  # Change this to your preferred AWS region
+}
+
 module "s3_dummy" {
   source      = "./modules/s3"
   bucket_name = "silver-guide-test"
